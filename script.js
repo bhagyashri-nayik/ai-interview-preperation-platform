28c7a64a309291bf628d58999195cda245a422af
Om <bhagyashrinayik7@gmail.com>
2026-06-10 23:43:15 +0530

    Added about section

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
 console.log("Login Page Loaded");
-const button = document.querySelector("button");
-
-button.addEventListener("click", function(){
-    alert("Welcome to AI Interview Platform!");
-});
